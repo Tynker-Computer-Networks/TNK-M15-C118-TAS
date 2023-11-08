3923ccf2d5de1c3a9347c2d1587b1836f7defec9
vishalgddam873 <vishal18@navgurukul.org>
2023-11-08 12:05:33 +0530

    Code Updated

--- TA2/main.py
+++ TA2/main.py
@@ -2,8 +2,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from tkinter import *
-# import messagebox and filedialog
-from tkinter import messagebox
+
 
 class EmailSenderApp(Tk):
     def __init__(self):
@@ -13,17 +12,23 @@ class EmailSenderApp(Tk):
         self.frame = Frame()
         self.frame.grid(column=0, row=0, padx=10, pady=10)
 
-        Label(self.frame, text="Sender's Email:").grid(column=0, row=0, sticky="w")
+        Label(self.frame, text="Sender's Email:").grid(
+            column=0, row=0, sticky="w")
         self.sender_email_entry = Entry(self.frame)
-        self.sender_email_entry.grid(column=1, row=0, padx=10, pady=5, columnspan=2)
+        self.sender_email_entry.grid(
+            column=1, row=0, padx=10, pady=5, columnspan=2)
 
-        Label(self.frame, text="Sender's Password:").grid(column=0, row=1, sticky="w")
+        Label(self.frame, text="Sender's Password:").grid(
+            column=0, row=1, sticky="w")
         self.sender_password_entry = Entry(self.frame, show="*")
-        self.sender_password_entry.grid(column=1, row=1, padx=10, pady=5, columnspan=2)
+        self.sender_password_entry.grid(
+            column=1, row=1, padx=10, pady=5, columnspan=2)
 
-        Label(self.frame, text="Recipient's Email:").grid(column=0, row=2, sticky="w")
+        Label(self.frame, text="Recipient's Email:").grid(
+            column=0, row=2, sticky="w")
         self.recipient_email_entry = Entry(self.frame)
-        self.recipient_email_entry.grid(column=1, row=2, padx=10, pady=5, columnspan=2)
+        self.recipient_email_entry.grid(
+            column=1, row=2, padx=10, pady=5, columnspan=2)
 
         Label(self.frame, text="Subject:").grid(column=0, row=3, sticky="w")
         self.subject_entry = Entry(self.frame)
@@ -31,7 +36,8 @@ class EmailSenderApp(Tk):
 
         Label(self.frame, text="Message:").grid(column=0, row=4, sticky="w")
         self.message_body_text = Text(self.frame, width=40, height=10)
-        self.message_body_text.grid(column=1, row=4, padx=10, pady=5, columnspan=2)
+        self.message_body_text.grid(
+            column=1, row=4, padx=10, pady=5, columnspan=2)
 
         # Create a Button with the Attach File.
         self.attach_button = Button(self.frame, text="Attach File")
@@ -39,11 +45,13 @@ class EmailSenderApp(Tk):
 
         # Create a Label with the Attached Files.
         self.attached_files_label = Label(self.frame, text="Attached Files:")
-        self.attached_files_label.grid(column=0, row=8, sticky="w", padx=10, pady=5)
+        self.attached_files_label.grid(
+            column=0, row=8, sticky="w", padx=10, pady=5)
 
-        self.send_button = Button(self.frame, text="Send Email")
+        # Call send_single_email function on button click
+        self.send_button = Button(
+            self.frame, text="Send Email", command=self.send_single_email)
         self.send_button.grid(column=1, row=9, padx=10, pady=10, columnspan=2)
-
 
     def send_single_email(self):
         #  Replace input with tkinter label values
@@ -58,7 +66,7 @@ class EmailSenderApp(Tk):
         recipient_email = self.recipient_email_entry.get()
         subject = self.subject_entry.get()
         message_body = self.message_body_text.get("1.0", "end")
-        
+
         try:
             smtp_server = smtplib.SMTP("smtp.gmail.com", 587)
             smtp_server.starttls()
@@ -69,21 +77,20 @@ class EmailSenderApp(Tk):
             message["To"] = recipient_email
             message["Subject"] = subject
             message.attach(MIMEText(message_body, "plain"))
-      
-            smtp_server.sendmail(sender_email, recipient_email, message.as_string())
+
+            smtp_server.sendmail(
+                sender_email, recipient_email, message.as_string())
             smtp_server.quit()
-            # Replace print with messagebox.showinfo()
-            messagebox.showinfo("Email Sent", "Email sent successfully!")
+            print("Email Sent", "Email sent successfully!")
 
         except Exception as e:
-            # Replace print with messagebox.showerror()
-            messagebox.showerror("Error", f"An error occurred: {str(e)}")
+            print("Error", f"An error occurred: {str(e)}")
 
 
 def main():
     app = EmailSenderApp()
     app.mainloop()
 
+
 if __name__ == "__main__":
     main()
-
--- TA3/main.py
+++ TA3/main.py
@@ -2,9 +2,11 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from tkinter import *
+# Import messagebox from tkinter
 from tkinter import messagebox
 # Import threading library
 import threading
+
 
 class EmailSenderApp(Tk):
     def __init__(self):
@@ -14,17 +16,23 @@ class EmailSenderApp(Tk):
         self.frame = Frame()
         self.frame.grid(column=0, row=0, padx=10, pady=10)
 
-        Label(self.frame, text="Sender's Email:").grid(column=0, row=0, sticky="w")
+        Label(self.frame, text="Sender's Email:").grid(
+            column=0, row=0, sticky="w")
         self.sender_email_entry = Entry(self.frame)
-        self.sender_email_entry.grid(column=1, row=0, padx=10, pady=5, columnspan=2)
+        self.sender_email_entry.grid(
+            column=1, row=0, padx=10, pady=5, columnspan=2)
 
-        Label(self.frame, text="Sender's Password:").grid(column=0, row=1, sticky="w")
+        Label(self.frame, text="Sender's Password:").grid(
+            column=0, row=1, sticky="w")
         self.sender_password_entry = Entry(self.frame, show="*")
-        self.sender_password_entry.grid(column=1, row=1, padx=10, pady=5, columnspan=2)
+        self.sender_password_entry.grid(
+            column=1, row=1, padx=10, pady=5, columnspan=2)
 
-        Label(self.frame, text="Recipient's Email:").grid(column=0, row=2, sticky="w")
+        Label(self.frame, text="Recipient's Email:").grid(
+            column=0, row=2, sticky="w")
         self.recipient_email_entry = Entry(self.frame)
-        self.recipient_email_entry.grid(column=1, row=2, padx=10, pady=5, columnspan=2)
+        self.recipient_email_entry.grid(
+            column=1, row=2, padx=10, pady=5, columnspan=2)
 
         Label(self.frame, text="Subject:").grid(column=0, row=3, sticky="w")
         self.subject_entry = Entry(self.frame)
@@ -32,16 +40,19 @@ class EmailSenderApp(Tk):
 
         Label(self.frame, text="Message:").grid(column=0, row=4, sticky="w")
         self.message_body_text = Text(self.frame, width=40, height=10)
-        self.message_body_text.grid(column=1, row=4, padx=10, pady=5, columnspan=2)
+        self.message_body_text.grid(
+            column=1, row=4, padx=10, pady=5, columnspan=2)
 
         self.attach_button = Button(self.frame, text="Attach File")
         self.attach_button.grid(column=0, row=7, padx=10, pady=5)
 
         self.attached_files_label = Label(self.frame, text="Attached Files:")
-        self.attached_files_label.grid(column=0, row=8, sticky="w", padx=10, pady=5)
+        self.attached_files_label.grid(
+            column=0, row=8, sticky="w", padx=10, pady=5)
 
         # Call send_email() function on button click
-        self.send_button = Button(self.frame, text="Send Email", command=self.send_email)
+        self.send_button = Button(
+            self.frame, text="Send Email", command=self.send_email)
         self.send_button.grid(column=1, row=9, padx=10, pady=10, columnspan=2)
 
     # Define send_email() function
@@ -55,7 +66,7 @@ class EmailSenderApp(Tk):
         recipient_email = self.recipient_email_entry.get()
         subject = self.subject_entry.get()
         message_body = self.message_body_text.get("1.0", "end")
-        
+
         try:
             smtp_server = smtplib.SMTP("smtp.gmail.com", 587)
             smtp_server.starttls()
@@ -66,12 +77,15 @@ class EmailSenderApp(Tk):
             message["To"] = recipient_email
             message["Subject"] = subject
             message.attach(MIMEText(message_body, "plain"))
-      
-            smtp_server.sendmail(sender_email, recipient_email, message.as_string())
+
+            smtp_server.sendmail(
+                sender_email, recipient_email, message.as_string())
             smtp_server.quit()
+            # Replace print with messagebox.showinfo()
             messagebox.showinfo("Email Sent", "Email sent successfully!")
 
         except Exception as e:
+            # Replace print with messagebox.showerror()
             messagebox.showerror("Error", f"An error occurred: {str(e)}")
 
 
@@ -79,7 +93,6 @@ def main():
     app = EmailSenderApp()
     app.mainloop()
 
+
 if __name__ == "__main__":
     main()
-
-
